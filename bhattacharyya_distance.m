a35ea877e59855792dea21863d4ab1752d0c7b49
Robert Evans <rlevans91@gmail.com>
2014-02-12 18:34:27 +0000

    Fixed histogram calculation and comparison

--- bhattacharyya_distance.m
+++ bhattacharyya_distance.m
@@ -6,7 +6,7 @@ function bdist = bhattacharyya_distance(histogram1, histogram2)
 
 % get number of bins
 % (i.e. dimension 2 from Nx1 inputs)
-bins = size(histogram1, 2);
+bins = size(histogram1, 1);
 
 % estimate the bhattacharyya co-efficient
 bcoeff = 0;
@@ -14,7 +14,7 @@ bcoeff = 0;
 for i=1:bins
     bcoeff = bcoeff + sqrt(histogram1(i) * histogram2(i));    
 end
-
+bdist = bcoeff;
 % get the distance between the two distributions as follows
 bdist = sqrt(1 - bcoeff);
 end

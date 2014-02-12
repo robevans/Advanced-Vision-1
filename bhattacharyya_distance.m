c8ee02838c46cf6bbeba70c556f2c3abcaa98e49
Robert Evans <rlevans91@gmail.com>
2014-02-12 20:18:44 +0000

    Comment

--- bhattacharyya_distance.m
+++ bhattacharyya_distance.m
@@ -2,6 +2,9 @@ function bdist = bhattacharyya_distance(histogram1, histogram2)
 % This function computes the bhattacharyya distance between two normalised
 % histograms.
 
+% Computes the bhattacharyya coefficient
 bcoeff = sqrt(histogram1(:)')*sqrt(histogram2(:));
+
+% Uses the coefficient to compute the bhattacharyya distance.
 bdist = sqrt(1 - bcoeff);
 end

f982092ee29c1de31457bed78ec9753195399744
Robert Evans <rlevans91@gmail.com>
2014-02-12 18:08:30 +0000

    Alternative way to calculate

--- bhattacharyya_distance.m
+++ bhattacharyya_distance.m
@@ -2,6 +2,19 @@ function bdist = bhattacharyya_distance(histogram1, histogram2)
 % This function computes the bhattacharyya distance between two normalised
 % histograms.
 
-bcoeff = sqrt(histogram1(:)')*sqrt(histogram2(:));
+%bcoeff = sqrt(histogram1(:)')*sqrt(histogram2(:));
+
+% get number of bins
+% (i.e. dimension 2 from Nx1 inputs)
+bins = size(histogram1, 2);
+
+% estimate the bhattacharyya co-efficient
+bcoeff = 0;
+
+for i=1:bins
+    bcoeff = bcoeff + sqrt(histogram1(i) * histogram2(i));    
+end
+
+% get the distance between the two distributions as follows
 bdist = sqrt(1 - bcoeff);
 end
